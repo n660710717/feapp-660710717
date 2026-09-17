ac342b0136f0a9f5c42183111166f4f2359b7492
NARUEMON WERUWIRIYAKUL <weruwiriyakul_n@su.ac.th>
2026-09-17 07:30:22 +0000

    lab8

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 import { movies } from '../data';
 import MovieCard from '../components/MovieCard';
 import MovieGallery from '../components/MovieGallery';
